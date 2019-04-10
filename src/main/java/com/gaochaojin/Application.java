13f4a95a41609e84d1773b1196320c1ebf8969ce
gaochaojin <gaochaojin@bbdservice.com>
2019-04-10 14:23:31 +0800

    init project

---
+++ src/main/java/com/gaochaojin/Application.java
@@ -0,0 +1,25 @@
+package com.gaochaojin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @Author: gaochaojin
+ * @Email: gaochaojin@163.com
+ * @Description:
+ * @Date Created in 9:43 2019/3/11
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+}
